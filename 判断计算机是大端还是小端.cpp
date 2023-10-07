a370e4f39f026402a79a58cf527dcbeee90ea0ab
chenxiaoshumiao <774764749@qq.com>
2023-10-07 11:48:40 +0800

    判断计算机是大端还是小端.cpp

---
+++ 判断计算机是大端还是小端.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+	int a = 1;
+	/*char* p =(char*) & a;
+	if (*p == 1) {
+		printf("小端");
+	}
+	else
+		printf("大端");
+	return 0;*/
+	if (*(char*)&a==1) {
+		printf("小端");
+	}
+	else
+		printf("大端");
+	return 0;
+
+}
